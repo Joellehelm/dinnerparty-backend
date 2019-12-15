9ee04ea1a1f885b35a1708389a581beaa724d40b
Joellehelm <joellenhelm@gmail.com>
2019-12-14 18:37:04 -0600

    working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ end
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token({ user_id: @user.id })
-      render json: { user: @user, jwt: @token, status: :created }
+      render json: { user: @user, jwt: @token, status: :created}
     else
       render json: { error: 'failed to create user', status: :not_acceptable}
     end
